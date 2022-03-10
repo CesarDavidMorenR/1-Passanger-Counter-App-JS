020ce5799e77a80789b6f87d96b26ff2212ad9c9
cDavid MorenoR <cesardaviddev@gmail.com>
2022-03-10 19:19:12 +0100

    sdsd

---
+++ index.js
@@ -0,0 +1,41 @@
+// comentar en una linea "//" document.getElementById("count-el").innerText=5
+
+let count = 1
+
+console.log(count)
+
+/* javaScript lee de arriba a abajo */
+
+let myAge = 27
+console.log(myAge)
+
+//Sumar dos valores y asignarlo a una variable 
+
+let firstBatch=5
+let secondBatch=5
+
+let suma=firstBatch+secondBatch
+console.log(suma)
+
+//Multiplicar dos valores
+
+let myAge2 =27
+let humanDogRatio=7
+
+let myDogAge=myAge2*humanDogRatio
+
+console.log(myDogAge)
+
+// Sumar valor a una variable
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 100
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints-25
+console.log(bonusPoints)
+
+bonusPoints=bonusPoints+70
+console.log(bonusPoints)
---
+++ readme.md
@@ -0,0 +1,18 @@
+#Para subir repositorios a Github desde git 
+
+…or create a new repository on the command line
+
+echo "# 1-Passanger-Counter-App-JS" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DavidMorenoDev/1-Passanger-Counter-App-JS.git
+git push -u origin main
+
+
+…or push an existing repository from the command line
+
+git remote add origin https://github.com/DavidMorenoDev/1-Passanger-Counter-App-JS.git
+git branch -M main
+git push -u origin main
