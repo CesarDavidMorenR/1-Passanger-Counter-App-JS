6f34b21a9982cfe95055a4dafbf2439427b3a832
cDavid MorenoR <cesardaviddev@gmail.com>
2022-03-22 20:24:28 +0100

    nueva prueba js

--- index.js
+++ index.js
@@ -92,3 +92,27 @@ function subirCont() {
    contaador = contaador + 1
    countEl.innerText = contaador
 }
+
+/* const firstMessage = 'I will print!';
+const secondMessage = 'I will not print!'; 
+ 
+// A do while with a stopping condition that evaluates to false
+do {
+ console.log(firstMessage)
+} while (true === false);
+ 
+// A while loop with a stopping condition that evaluates to false
+while (true === false){
+  console.log(secondMessage)
+};
+  */
+
+/* let cupsOfSugarNeeded = 777
+let cupsAdded = 0
+
+do {
+   cupsAdded++
+   console.log(cupsAdded)
+   
+   } while (cupsAdded < cupsOfSugarNeeded)
+    */
